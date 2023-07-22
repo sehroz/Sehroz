52feeab99d7c8ce300b5e26c55ed5282cc3b197b
unknown <info@sehroz.com>
2023-07-22 15:45:05 -0400

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,68 @@
+import "./globals.css";
+import { Inter } from "@next/font/google";
+import LocalFont from "@next/font/local";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: "sehroz.com",
+    template: "%s | sehroz.com",
+  },
+  description:
+    "Web developer at alphaappraisals.ca,condosx.ca and founder of stackedtowns.com",
+  openGraph: {
+    title: "sehroz.com",
+    description:
+      "Web developer at alphaappraisals.ca,condosx.ca and founder of stackedtowns.com",
+    url: "https://sehroz.com",
+    siteName: "sehroz.com",
+    images: [
+      {
+        url: "https://chronark.com/og.png",
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "SehrozKhan",
+    card: "summary_large_image",
+  },
+  icons: {
+    shortcut: "/favicon.png",
+  },
+};
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const calSans = LocalFont({
+  src: "../public/fonts/CalSans-SemiBold.ttf",
+  variable: "--font-calsans",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+      <body className={`bg-black`}> {children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import React from "react";
+import Particles from "../components/ui/particles";
+
+const navigation = [];
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-slate-900 via-zinc-600/20 to-yellow-500">
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <Particles
+        className="absolute inset-0 -z-10 animate-fade-in"
+        quantity={100}
+      />
+      <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+        Sehroz
+      </h1>
+
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className=" text-center animate-fade-in grid h-24">
+        <h2 className="text-sm text-zinc-500 ">
+          Hi, my name is Sehroz. Let's building something.
+        </h2>
+        <Link
+          target="_blank"
+          href="https://alphaappraisals.ca"
+          className="  text-xl underline duration-500 text-zinc-300"
+        >
+          Recent Work
+        </Link>
+      </div>
+    </div>
+  );
+}
