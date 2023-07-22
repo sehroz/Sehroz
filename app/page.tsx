890015b3e8fa944d1f96da55001199b779fa7057
unknown <info@sehroz.com>
2023-07-22 15:51:17 -0400

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const navigation = [];
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-slate-900 via-zinc-600/20 to-yellow-500">
+    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-slate-900 via-zinc-600/20 to-yellow-300">
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
@@ -18,24 +18,26 @@ export default function Home() {
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className=" text-center animate-fade-in grid h-24">
-        <h2 className="text-sm text-zinc-500 ">
+        <h2 className="text-sm text-zinc-100 ">
           Hi, my name is Sehroz. Let`s building something.
         </h2>
-        <h1>Recent Work</h1>
-        <Link
-          target="_blank"
-          href="https://alphaappraisals.ca"
-          className="  text-xl underline duration-500 text-zinc-300"
-        >
-          Alpha Appraisals
-        </Link>
-        <Link
-          target="_blank"
-          href="https://condosx.ca"
-          className="  text-xl underline duration-500 text-zinc-300"
-        >
-          CondosX
-        </Link>
+        <h1 className=" text-xl text-yellow-500 pt-10">View Recent Projects</h1>
+        <div className="flex justify-evenly pt-2">
+          <Link
+            target="_blank"
+            href="https://www.alphaappraisals.ca"
+            className=" hover:font-bold underline duration-500 text-zinc-300"
+          >
+            Alpha Appraisals
+          </Link>
+          <Link
+            target="_blank"
+            href="https://www.condosx.ca"
+            className=" hover:font-bold underline duration-500 text-zinc-300"
+          >
+            CondosX
+          </Link>
+        </div>
       </div>
     </div>
   );
