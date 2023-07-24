0e9cf1be4ae62477b3ad95de948dabd9fe6d181e
unknown <info@sehroz.com>
2023-07-23 23:15:23 -0400

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />{" "}
-      <div className=" w-4/12 z-10 relative">
-        <h2 className="text-xl font-bold text-zinc-100">Hey, I am </h2>{" "}
+      <div className=" z-10 relative">
+        <h2 className="text-xl font-extrabold text-zinc-100">Hey, I am </h2>{" "}
       </div>
       <h1 className="z-10 text-7xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text relative">
         <span className="gradient-animation">Sehroz.</span>
@@ -24,19 +24,21 @@ export default function Home() {
       </h1>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="  animate-fade-in grid text-center p-10 ">
-        <h1 className=" text-sm  font-light text-white pt-5">My Recent Work</h1>
+        <h1 className=" text-sm  font-bold text-white pt-5">My Recent Work</h1>
         <div className="flex w-screen sm:w-fit align-middle justify-evenly   items-center pt-2 text-xl font-thin">
           <Link
             target="_blank"
             href="https://www.alphaappraisals.ca"
-            className=" hover:font-bold  underline duration-500 text-white px-10"
+            className=" hover:font-bold  animate-pulse duration-500 text-white px-10"
           >
-            Alpha <br /> Appraisals
+            Alpha
+            <br />
+            Appraisals
           </Link>{" "}
           <Link
             target="_blank"
             href="https://www.condosx.ca"
-            className=" hover:font-bold underline duration-500 text-white px-10"
+            className=" hover:font-bold  animate-pulse  duration-500 text-white px-10"
           >
             CONDOSX
           </Link>
@@ -47,11 +49,17 @@ export default function Home() {
           target="_blank"
           className=" w-full text-md font-bold text-yellow-500 pt-10 text-center align-middle items-center center justify-center"
         >
-          <svg viewBox="0 0 24 24" fill="white" height="1.5em" width="auto">
+          linkedin.com/in/sehroz <br />
+          <span className="font-light"> Toronto </span>{" "}
+          <svg
+            className="rotate-180"
+            viewBox="0 0 24 24"
+            fill="white"
+            height="1.5em"
+            width="auto"
+          >
             <path d="M12 10.8a2 2 0 10-2-2 2 2 0 002 2zm-.71 6.91a1 1 0 001.42 0l4.09-4.1a6.79 6.79 0 10-9.6 0zM7.23 8.34a4.81 4.81 0 012.13-3.55 4.81 4.81 0 015.28 0 4.82 4.82 0 01.75 7.41L12 15.59 8.61 12.2a4.77 4.77 0 01-1.38-3.86zM19 20H5a1 1 0 000 2h14a1 1 0 000-2z" />
           </svg>{" "}
-          linkedin.com/in/sehroz <br />
-          <span className="font-light"> Toronto, Ontario </span>
         </a>
       </div>
     </div>
